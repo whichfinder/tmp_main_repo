b239e216f24215dc656a8bc102c006d79f85fa0e
Sasha <okura@softserveinc.com>
2022-04-29 18:52:29 +0300

    upd8

--- update_submodules.sh
+++ update_submodules.sh
@@ -6,7 +6,4 @@ if [[ ! -z $1 ]]
         branch="main"
 fi
 echo $branch;
-for item in $(echo */)
-do
-    cd $item && git submodule init && git submodule update && git pull origin $branch && cd ..
-done;
+git submodule foreach && git submodule init && git submodule update && git pull origin $branch
